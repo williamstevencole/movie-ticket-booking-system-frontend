508f35f42be7302eaa13273cba91455e303529ef
Andrea Quin <andrea.quin123@gmail.com>
2026-06-20 11:07:02 -0600

    format commands

--- src/app/features/cancelacion/cancelar/cancelar.component.ts
+++ src/app/features/cancelacion/cancelar/cancelar.component.ts
@@ -5,7 +5,6 @@ import { AppbarComponent } from '../../../shared/components/appbar/appbar.compon
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
 import { ActivatedRoute, Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-cancelar',
   standalone: true,
@@ -14,14 +13,11 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './cancelar.component.scss',
 })
 export class CancelarComponent implements OnInit {
-
-
   constructor(
     private route: ActivatedRoute,
     private location: Location,
     private router: Router,
   ) {}
-
 
   readonly nav = [
     { label: 'Cartelera', route: '/' },
@@ -31,51 +27,36 @@ export class CancelarComponent implements OnInit {
     { label: 'Mis boletos', route: '/mis-boletos', active: true },
   ];
 
-
   id!: string;
 
   reserva!: BoletoMock;
 
   cancelado = false;
 
-
   ngOnInit() {
-
     this.id = this.route.snapshot.paramMap.get('id')!;
 
     console.log('Cancelando reserva:', this.id);
 
-
-    this.reserva = MOCK_BOLETOS.find(
-      b => b.id === this.id
-    )!;
-
+    this.reserva = MOCK_BOLETOS.find((b) => b.id === this.id)!;
   }
 
-
   confirmarCancelacion() {
-
     // aquí después va el backend
     console.log('Cancelando:', this.reserva.id);
 
-
     this.cancelado = true;
-
 
     setTimeout(() => {
       this.volverBoletos();
     }, 2000);
-
   }
-
 
   volver() {
     this.location.back();
   }
 
-
   volverBoletos() {
     this.router.navigate(['/mis-boletos']);
   }
-
 }
