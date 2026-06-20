7d601fe678a2e0db5c68be2af19b3ba295cd0a18
Andrea Quin <andrea.quin123@gmail.com>
2026-06-20 11:06:13 -0600

    added a modal when clicking cancel event is happening

--- src/app/features/cancelacion/cancelar/cancelar.component.ts
+++ src/app/features/cancelacion/cancelar/cancelar.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
-import { MOCK_BOLETOS } from '../../../mocks/data/boletos.mock';
+import { MOCK_BOLETOS, BoletoMock } from '../../../mocks/data/boletos.mock';
 import { AppbarComponent } from '../../../shared/components/appbar/appbar.component';
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-cancelar',
@@ -12,8 +13,16 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './cancelar.component.html',
   styleUrl: './cancelar.component.scss',
 })
-export class CancelarComponent {
-  constructor(private route: ActivatedRoute, private location: Location) {}
+export class CancelarComponent implements OnInit {
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router,
+  ) {}
+
+
   readonly nav = [
     { label: 'Cartelera', route: '/' },
     { label: 'Próximos estrenos' },
@@ -22,15 +31,51 @@ export class CancelarComponent {
     { label: 'Mis boletos', route: '/mis-boletos', active: true },
   ];
 
+
   id!: string;
 
-  reserva = MOCK_BOLETOS.find((b) => b.id === this.id);
+  reserva!: BoletoMock;
+
+  cancelado = false;
+
+
+  ngOnInit() {
+
+    this.id = this.route.snapshot.paramMap.get('id')!;
+
+    console.log('Cancelando reserva:', this.id);
+
+
+    this.reserva = MOCK_BOLETOS.find(
+      b => b.id === this.id
+    )!;
+
+  }
+
+
+  confirmarCancelacion() {
+
+    // aquí después va el backend
+    console.log('Cancelando:', this.reserva.id);
+
+
+    this.cancelado = true;
+
+
+    setTimeout(() => {
+      this.volverBoletos();
+    }, 2000);
+
+  }
+
 
   volver() {
     this.location.back();
   }
 
-  ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id')!;
+
+  volverBoletos() {
+    this.router.navigate(['/mis-boletos']);
   }
+
 }
